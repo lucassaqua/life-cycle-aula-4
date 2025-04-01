b36468efd4a82dbaf9ca9511c72c33ec3aabad4d
Lucas <lucassaqua@live.com>
2025-04-01 16:02:26 -0300

    commit inicial

---
+++ src/app/components/item/item.component.ts
@@ -0,0 +1,27 @@
+import { outputAst } from '@angular/compiler';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output } from '@angular/core';
+import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { Item } from 'src/app/interfaces/iItem';
+
+@Component({
+  selector: 'app-item',
+  templateUrl: './item.component.html',
+  styleUrls: ['./item.component.css']
+})
+export class ItemComponent implements OnInit, OnChanges {
+  @Input() item!: Item;
+  @Output() emitindoItemParaEditar = new EventEmitter();
+
+  faPen = faPen;
+  faTrash = faTrash
+
+  constructor() { }
+
+  ngOnInit(): void { }
+
+  ngOnChanges(){ }
+
+  editarItem(){
+    this.emitindoItemParaEditar.emit(this.item);
+  }
+}
